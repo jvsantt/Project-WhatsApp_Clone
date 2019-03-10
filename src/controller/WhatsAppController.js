9d82f345c2377448d0776c40550eed825c7128d1
João Santana <joaovictorsantt@gmail.com>
2019-03-10 19:37:28 -0300

    new update

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -159,6 +159,7 @@ class WhatsAppController {
       this.el.panelCamera.css({
         height: "calc(100% - 120px)"
       });
+      this._camera = new CameraController(this.videoCamera);
     });
 
     this.el.btnClosePanelCamera.on("click", e => {
@@ -247,7 +248,25 @@ class WhatsAppController {
           img.classList.add(name);
         });
 
-        this.el.inputText.appendChild(img);
+        let cursor = window.getSelection();
+        if (!cursor.focusNode || !cursor.focusNode.id == "input-text") {
+          this.el.inputText.focus();
+          cursor = window.getSelection();
+        }
+
+        let range = document.createRange();
+
+        range = cursor.getRangeAt(0);
+        range.deleteContents();
+
+        let frag = document.createDocumentFragment();
+
+        frag.appendChild(img);
+
+        range.insertNode(frag);
+
+        range.setStartAfter(img);
+
         this.el.inputText.dispatchEvent(new Event("keyup"));
       });
     });

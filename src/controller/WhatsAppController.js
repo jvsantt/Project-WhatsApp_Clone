e6e68b64eef94da7156d696ebcd3f4d9f595efc0
João Santana <joaovictorsantt@gmail.com>
2019-03-11 17:16:28 -0300

    new update

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -1,4 +1,8 @@
-class WhatsAppController {
+import { Format } from "./../util/Format";
+import { CameraController } from "./CameraController";
+import { DocumentPreviewController } from "./DocumentPreviewController";
+
+export class WhatsAppController {
   constructor() {
     console.log("WhatsAppController OK");
 
@@ -159,16 +163,36 @@ class WhatsAppController {
       this.el.panelCamera.css({
         height: "calc(100% - 120px)"
       });
-      this._camera = new CameraController(this.videoCamera);
+      this._camera = new CameraController(this.el.videoCamera);
     });
 
     this.el.btnClosePanelCamera.on("click", e => {
       this.closeAllMainPanel();
       this.el.panelMessagesContainer.show();
+      this._camera.stop();
     });
 
     this.el.btnTakePicture.on("click", e => {
-      console.log("take picture");
+      let dataURL = this._camera.takePicture();
+
+      this.el.pictureCamera.src = dataURL;
+      this.el.pictureCamera.show();
+      this.el.videoCamera.hide();
+      this.el.btnReshootPanelCamera.show();
+      this.el.containerTakePicture.hide();
+      this.el.containerSendPicture.show();
+    });
+
+    this.el.btnReshootPanelCamera.on("click", e => {
+      this.el.pictureCamera.hide();
+      this.el.videoCamera.show();
+      this.el.btnReshootPanelCamera.hide();
+      this.el.containerTakePicture.show();
+      this.el.containerSendPicture.hide();
+    });
+
+    this.el.btnSendPicture.on("click", e => {
+      console.log(this.el.panelCamera.src);
     });
 
     this.el.btnAttachDocument.on("click", e => {
@@ -177,6 +201,56 @@ class WhatsAppController {
       this.el.panelDocumentPreview.css({
         height: "calc(100% - 120px)"
       });
+
+      this.el.inputDocument.click();
+    });
+
+    this.el.inputDocument.on("change", e => {
+      if (this.el.inputDocument.files.length) {
+        let file = this.el.inputDocument.files[0];
+        this._documentPreviewController = new DocumentPreviewController(file);
+        this._documentPreviewController
+          .getPreviewData()
+          .then(result => {
+            this.el.imgPanelDocumentPreview.src = result.src;
+            this.el.infoPanelDocumentPreview.innerHTML = result.info;
+            this.el.imagePanelDocumentPreview.show();
+            this.el.filePanelDocumentPreview.hide();
+          })
+          .catch(err => {
+            console.log(file.type);
+            switch (file.type) {
+              case "application/vnd.ms-excel":
+              case "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
+                this.el.iconPanelDocumentPreview.className =
+                  "jcxhw icon-doc-xls";
+
+                break;
+
+              case "application/vnd.ms-powerpoint":
+              case "application/vnd.openxmlformats-officedocument.presentationml.presentation":
+                this.el.iconPanelDocumentPreview.className =
+                  "jcxhw icon-doc-ppt";
+
+                break;
+
+              case "application/msword":
+              case "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
+                this.el.iconPanelDocumentPreview.className =
+                  "jcxhw icon-doc-doc";
+
+                break;
+
+              default:
+                this.el.iconPanelDocumentPreview.className =
+                  "jcxhw icon-doc-generic";
+                break;
+            }
+            this.el.filenamePanelDocumentPreview.innerHTML = file.name;
+            this.el.imagePanelDocumentPreview.hide();
+            this.el.filePanelDocumentPreview.show();
+          });
+      }
     });
 
     this.el.btnClosePanelDocumentPreview.on("click", e => {

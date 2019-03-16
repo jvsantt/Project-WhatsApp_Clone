4d24f97ebc0d3950497e1467fc145b0280abde56
João Santana <joaovictorsantt@gmail.com>
2019-03-16 17:30:21 -0300

    new update

--- src/model/User.js
+++ src/model/User.js
@@ -50,4 +50,33 @@ export class User extends Model {
   static findByEmail(email) {
     return User.getRef().doc(email);
   }
+
+  static getContactsRef(id) {
+    return User.getRef()
+      .doc(id)
+      .collection("contacts");
+  }
+
+  addContact(contact) {
+    return User.getContactsRef(this.email)
+      .doc(btoa(contact.email))
+      .set(contact.toJSON());
+  }
+
+  getContacts() {
+    return new Promise((s, f) => {
+      User.getContactsRef(this.email).onSnapshot(docs => {
+        let contacts = [];
+        docs.forEach(doc => {
+          let data = doc.data();
+
+          data.id = doc.id;
+
+          contacts.push(data);
+        });
+        this.trigger("contactschange", docs);
+        s(contacts);
+      });
+    });
+  }
 }
